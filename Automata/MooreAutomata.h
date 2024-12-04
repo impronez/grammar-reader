ab424d4c496ff6316c70ec398a128dfc5c9d6074
impronez <orzaev7312@gmail.com>
2024-12-04 14:50:02 +0300

    Убрал лишнее

--- Automata/MooreAutomata.h
+++ Automata/MooreAutomata.h
@@ -194,11 +194,6 @@ private:
             newStateNames[state] = NEW_STATE_CHAR + std::to_string(i++);
         }
 
-        for (auto& it: newStateNames)
-        {
-            std::cout << it.first << ": " << it.second << std::endl;
-        }
-
         return newStateNames;
     }
 
